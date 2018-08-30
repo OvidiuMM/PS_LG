c8fd152ccf1c9a87add8d986db6560c8fcee75c0
Ovidiu Mircea Moldovan <ovidiu5891@gmail.com>
2018-08-30 12:02:04 +0200

    just before graphing state

--- Ejercicio2/js/dataParser.js
+++ Ejercicio2/js/dataParser.js
@@ -5,88 +5,161 @@
     * use graph library to display */
 
 var myArticle = document.getElementById('toReplaceWithGraphs');
-var processed_data = []
-
-function process_data1(result){
+var processedData = []
+var fileCounter = 0;
+var debugMode = true;
+function processData1(result){
+  if (debugMode){
+    console.log("Process Data 1 function");
+  }
   //parse data set 1
-    for (data_field of result){
+    for (dataField of result){
       //for each filed store cat, date and val (with proper format)
-        var seconds_d = data_field.d.toString();
+        var seconds_d = dataField.d.toString();
         seconds_d = seconds_d.slice(0, -3);
-        let thenum = data_field.cat.match(/\d+/)[0];
+        let thenum = dataField.cat.match(/\d+/)[0];
         let category = "CAT " + thenum;
         function toDateTime(secs) {
           var t = new Date(1970, 0, 1); // Epoch
           t.setSeconds(secs);
-          month = t.getMonth() < 10 ? '0' + t.getMonth() : '' + t.getMonth();
+
+          month = (t.getMonth() + 1) < 10 ? '0' + (t.getMonth() + 1) : '' + (t.getMonth() + 1);
           day = t.getDate() < 10 ? '0' + t.getDate(): '' + t.getDate();
           return t.getFullYear() + "-" + month  + "-" + day ;
         }
-        let processed_data_field = { "date": toDateTime(parseInt(seconds_d)), "category": "CAT " + thenum,
-        "value": parseFloat(data_field.value).toFixed(2)
+        let processedDataField = { "date": toDateTime(parseInt(seconds_d)), "category": "CAT " + thenum,
+        "value": parseFloat(dataField.value).toFixed(2)
       }
-      processed_data.push(processed_data_field);
-    }
 
+      function findIndex(element) {
+        return element.category == processedDataField.category
+        && element.date == processedDataField.date;
+      }
+
+      let posFound = processedData.findIndex(findIndex);
+
+      if (posFound != -1){
+            processedData[posFound].value = parseFloat(processedData[posFound].value)
+        + parseFloat(processedDataField.value);
+        if (debugMode){
+          console.log("coincidence found for elment of data set 1: ", processedDataField);
+        }
+      }
+      else{
+      processedData.push(processedDataField);
+      }
+      }
+
+      fileCounter++;
+    graphingData();
   }
 
-  function process_data2(result){
+  function processData2(result){
+    if (debugMode){
+      console.log("Process Data 2 function");
+    }
     //parse data set 2
-    for (data_field of result){
+    for (dataField of result){
       //for each filed store cat, date and val (with proper format)
-        let thenum = data_field.categ.match(/\d+/)[0];
+        let thenum = dataField.categ.match(/\d+/)[0];
         let category = "CAT " + thenum;
-        let processed_data_field = { "date": data_field.myDate, "category": "CAT " + thenum,
-        "value": parseFloat(data_field.val).toFixed(2)};
-        processed_data.push(processed_data_field);
-    }
+        let processedDataField = { "date": dataField.myDate, "category": "CAT " + thenum,
+        "value": parseFloat(dataField.val).toFixed(2)
+      }
+        function findIndex(element) {
+          return element.category == processedDataField.category
+          && element.date == processedDataField.date;
+        }
+        let posFound = processedData.findIndex(findIndex);
+
+        if (posFound != -1){
+          processedData[posFound].value = parseFloat(processedData[posFound].value)
+          + parseFloat(processedDataField.value);
+          if (debugMode){
+            console.log("coincidence found for elment of data set 2: ", processedDataField);
+          }
+        }
+        else{
+        processedData.push(processedDataField);
+        }
+        }
+
+      fileCounter++;
+      graphingData();
   }
 
-  function process_data3(result){
+  function processData3(result){
 
   //parse data set 3
 //  {"raw":"CHCZfl8dVb iAF1sqI 2015-06-03 rM0dh Rqh kNfs1H Z0LR3K6 xZqfc 2C359d
 //  foLx LB6c #CAT 2#","val":6.400865852071913}
-
-    for (data_field of result){
-      //  console.log("field:", data_field);
-      let thenum = data_field.raw.match(/#CAT \d+/)[0];
+if (debugMode){
+  console.log("Process Data 3 function");
+}
+    for (dataField of result){
+      //  console.log("field:", dataField);
+      let thenum = dataField.raw.match(/#CAT \d+/)[0];
       thenum = thenum.match(/\d+/)[0];
-      let date_field = data_field.raw.match(/\d{4}\-\d{2}\-\d{2}/)[0];
+      let dateField = dataField.raw.match(/\d{4}\-\d{2}\-\d{2}/)[0];
       let category = "CAT " + thenum;
-      let processed_data_field = { "date": date_field, "category": "CAT " + thenum,
-      "value": parseFloat(data_field.val).toFixed(2)
+      let processedDataField = { "date": dateField, "category": "CAT " + thenum,
+      "value": parseFloat(dataField.val).toFixed(2)
 
     }
-    processed_data.push(processed_data_field);
+    function findIndex(element) {
+      return element.category == processedDataField.category
+      && element.date == processedDataField.date;
     }
+    let posFound = processedData.findIndex(findIndex);
+      if (posFound != -1){
+        processedData[posFound].value = parseFloat(processedData[posFound].value)
+        + parseFloat(processedDataField.value);
 
-  console.log("data proces list:",processed_data);
+        if (debugMode){
+          console.log("coincidence found for elment of data set 3: ",processedDataField);
+        }
+    }
+    else{
+    processedData.push(processedDataField);
+    }
+    }
+  fileCounter++;
   graphingData();
 }
 
 function graphingData(){
-    console.log('Graphing data...');
+  if (debugMode){
+    console.log("Function grpahing data");
+  }
+  if (fileCounter == 3){
     toReplaceWithGraphs.innerHTML = "Data retrieved <br> Processing data files... <br> Graphing data...";
+    if (debugMode){
+      console.log("all data set obtained... grpahing ");
+    }
+    
+  }
 }
 
 function getDataFromURL(url){
+  if (debugMode){
+    console.log("get data function for url: ",url);
+  }
 return fetch(url).then(resp => resp.json())
 }
 
 function obtainJSON(){
+  getDataFromURL('https://s3.amazonaws.com/logtrust-static/test/test/data2.json')
+  .then(processData2)
+  .catch(function(error) {
+    console.log('Request failed', error);
+  });
   getDataFromURL('https://s3.amazonaws.com/logtrust-static/test/test/data1.json')
-  .then(process_data1)
+  .then(processData1)
   .catch(function(error) {
     console.log('Request failed', error);
   });
   getDataFromURL('https://s3.amazonaws.com/logtrust-static/test/test/data3.json')
-  .then(process_data3)
-  .catch(function(error) {
-    console.log('Request failed', error);
-  });
-  getDataFromURL('https://s3.amazonaws.com/logtrust-static/test/test/data2.json')
-  .then(process_data2)
+  .then(processData3)
   .catch(function(error) {
     console.log('Request failed', error);
   });
